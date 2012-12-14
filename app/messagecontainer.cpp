3a390dc5d7fcd212df96ce4b1747b347afcbecdc
Sven Aßmann <sven.assmann@lubico.biz>
2012-12-14 21:08:08 +0100

    - introduce decorator for wrapping and chaining message container - change encryption form core into a own message container decorator - support now encryption for windows v2 based stegno message (those without attachments) - small code cleanup for memory handling (some deletes where missing before)

--- app/messagecontainer.cpp
+++ app/messagecontainer.cpp
@@ -37,6 +37,15 @@ void MessageContainerBase::setBytes(const QByteArray& bytes) {
     emit textChanged(this->textBlock);
 }
 
+MessageContainerWrapper::MessageContainerWrapper(IMessageContainer* wrapee) :
+    wrapee(wrapee) { }
+    
+MessageContainerWrapper::~MessageContainerWrapper() {
+    if ( this->wrapee ) {
+        delete this->wrapee;
+    }
+}
+
 MessageContainerV0::MessageContainerV0()    { }
 MessageContainerV0::~MessageContainerV0()   { }
 
@@ -77,15 +86,124 @@ bool MessageContainerV1::isValidFormat(const QByteArray& bytes) {
         }
         return false;
     }
-    data[length] = '\0';
+    data[length] = 0x00;
     ms>>terminator;
     
-    if (version == this->Version /*&& terminator == this->Terminator*/ ) {
+    if (version == this->Version && terminator == this->Terminator ) {
         // data was allocated and must deleted later
         this->bytesBuffer = QByteArray(data, length);
         delete[] data;
+        
+        // here we fix a bug of windows version 2 Terminators are present
+        if(     this->bytesBuffer.length() > 0 
+            &&  (byte)this->bytesBuffer.at(this->bytesBuffer.length()-1) == 0xff
+        ){
+            this->bytesBuffer = this->bytesBuffer.left(this->bytesBuffer.length()-1);
+        }
+        if(     this->bytesBuffer.length() > 0 
+            &&  (byte)this->bytesBuffer.at(this->bytesBuffer.length()-1) == 0xff
+        ) {
+            this->bytesBuffer = this->bytesBuffer.left(this->bytesBuffer.length()-1);
+        }
+        // end of special bug fix
         this->textBlock = QString::fromUtf8(this->bytesBuffer);
         return true;
     }
     return false;
+}
+
+MessageContainerEncypted::MessageContainerEncypted(IMessageContainer* wrapee, const QByteArray& key, const QString& algorithm) : 
+    MessageContainerWrapper(wrapee),
+    version(0x03),
+    key(key),
+    algorithm(algorithm),
+    init(0),
+    cipher(0)
+{
+    this->init = new QCA::Initializer();
+    if (algorithm.isEmpty()) {
+        this->version = 0x03;
+        this->algorithm = QString("aes128");
+        this->algorithmType = this->algorithm + QString("-cbc-pkcs7");
+    }
+}
+
+MessageContainerEncypted::~MessageContainerEncypted() {
+    this->cipher    && delete this->cipher;
+    this->init      && delete this->init;
+}
+
+bool MessageContainerEncypted::isEncryptionSupported() {
+
+    if( !this->init ) {
+        this->init = new QCA::Initializer();
+    }
+
+    if(!QCA::isSupported(this->algorithmType.toAscii().constData())) {
+        qDebug("MessageContainerEncyptor::isEncryptionSupported: encryption algorithm %s with %s not supported!", 
+            this->algorithm.data_ptr(), 
+            this->algorithmType.data_ptr()
+        );
+        return false;
+    }
+    return true;
+}
+
+QCA::Cipher* MessageContainerEncypted::createCipher(QCA::Direction direction) {
+
+    if(!this->isEncryptionSupported()) {
+        return 0;
+    }
+    
+    if( this->cipher ) {
+        return this->cipher;
+    }
+    
+    QCA::SymmetricKey key(this->key);
+    QCA::InitializationVector iv(this->key);
+    this->cipher = new QCA::Cipher(
+        this->algorithm,
+        QCA::Cipher::CBC,
+        QCA::Cipher::PKCS7,
+        direction,
+        key, iv
+    );
+    
+    return this->cipher;    
+}
+
+
+QByteArray MessageContainerEncypted::bytes() {
+    QByteArray buf = MessageContainerWrapper::bytes();
+
+    if(!this->isEncryptionSupported()) {
+        return buf;
+    }
+    //buf.prepend(this->version);
+    
+    QCA::Cipher* cipher = this->createCipher(QCA::Encode);
+    //qDebug("MessageContainerEncyptor::bytes: message raw '%s'", qPrintable(QCA::arrayToHex(buf)));
+    QCA::SecureArray secretText = cipher->process(buf);
+    //qDebug("MessageContainerEncyptor::bytes: message encryped '%s'", qPrintable(QCA::arrayToHex(secretText.toByteArray())));
+    return secretText.toByteArray();    
+}
+
+bool MessageContainerEncypted::isValidFormat(const QByteArray& bytes) {
+
+    if(!this->isEncryptionSupported()) {
+        return MessageContainerWrapper::isValidFormat(bytes);
+    }
+
+    QByteArray myBytes = bytes;
+    //if (bytes.length() && (byte)bytes[0] == this->version) {
+    //    int len = bytes.length() - 1;
+    //    myBytes = bytes.right(len);
+    //
+    //}
+
+    QCA::Cipher* cipher = this->createCipher(QCA::Decode);
+    //qDebug("SteganoCore::decryptData: message raw '%s'", qPrintable(QCA::arrayToHex(buf)));
+    QCA::SecureArray clearText = cipher->process(myBytes);
+    //qDebug("SteganoCore::decryptData: message decryped '%s'", qPrintable(QCA::arrayToHex(clearText.toByteArray())));
+    return MessageContainerWrapper::isValidFormat(clearText.toByteArray());
 }
--- app/messagecontainer.h
+++ app/messagecontainer.h
@@ -4,6 +4,7 @@
 #include <QString>
 #include <QObject>
 #include <QByteArray>
+#include <QtCrypto/QtCrypto>
 
 typedef uchar byte;
 
@@ -51,7 +52,49 @@ protected:
     const byte  Version;
     const byte  Terminator;
     QByteArray  bytesBuffer;
+    IMessageContainer *wrapMe;
 
+};
+
+class MessageContainerWrapper : public IMessageContainer {
+    Q_OBJECT
+public:
+    /**
+     * TODO finalize that http://sourcemaking.com/design_patterns/decorator/cpp/2
+     */
+    MessageContainerWrapper(IMessageContainer* wrapee);
+    virtual ~MessageContainerWrapper();
+    
+    virtual QByteArray bytes()                          { return this->wrapee->bytes(); }
+    virtual bool isValidFormat(const QByteArray& bytes) { return this->wrapee->isValidFormat(bytes); }
+    virtual uint length()                               { return this->wrapee->length(); }                              
+    virtual void setBytes(const QByteArray& bytes)      { this->wrapee->setBytes(bytes); }
+    virtual void setText(const QString& text)           { this->wrapee->setText(text); }
+    virtual byte terminator()                           { return this->wrapee->terminator(); }
+    virtual QString text()                              { return this->wrapee->text(); }
+    virtual byte version()                              { return this->wrapee->version(); }
+    
+protected:
+    IMessageContainer* wrapee;    
+};
+
+class MessageContainerEncypted : public MessageContainerWrapper {
+    Q_OBJECT
+public:
+    MessageContainerEncypted(IMessageContainer* wrapee, const QByteArray& key, const QString& algorithm);
+    virtual             ~MessageContainerEncypted();
+    virtual QByteArray  bytes();
+    virtual bool        isValidFormat(const QByteArray& bytes);
+    bool                isEncryptionSupported();
+protected:
+    byte                version;
+    QByteArray          key;
+    QString             algorithm;
+    QString             algorithmType;
+    QCA::Cipher*        cipher;
+    
+    QCA::Initializer*   init;
+    QCA::Cipher*        createCipher(QCA::Direction direction);
 };
 
 class MessageContainerV0 : public MessageContainerBase {
